0385642e496a2fa0abb5f346c33bb52e0c258b0b
GabrielRodriguesMoreira <gabriel.rodrigues.moreira08@aluno.ifce.edu.br>
2023-10-02 20:44:55 -0300

    init

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,28 +2,28 @@ import { MdKeyboardArrowRight } from 'react-icons/md';
 
 export default function Home() {
   return (
-    <main className="flex flex-col p-4  lg:flex-row">
+    <main className="flex flex-col p-4  lg:flex-row lg:space-x-2 justify-center items-center">
       <section className="flex w-full flex-col items-center space-y-3 mb-10 lg:w-3/5">
         <img className='lg:w-full' src="title.png" alt="" />
         <img src="/gifimage.gif" alt="" />
         <p className="font-kalam text-pink-700 text-xl lg:text-2xl">&quot;Ensinando com <br></br> responsabilidade!&quot;</p>
         <button className="flex items-center justify-center space-x-5 bg-pink-600 p-4 w-full text-2xl text-white font-inter rounded-sm shadow-xl  lg:hidden"> <p>Garanta sua Vaga!</p> <span className='bg-white text-4xl text-pink-700 rounded-full'><MdKeyboardArrowRight /></span> </button>
       </section>
-      <section className='flex flex-col w-full items-center space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2  lg:gap-2'>
+      <section className='flex flex-col w-full items-center space-y-5 lg:space-y-0 lg:grid lg:grid-cols-2 lg:gap-2'>
         <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm'>
-          <img className='rounded-sm ' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
+          <img className='rounded-sm max-h-52 object-cover' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
           <p>10 anos de experiência!</p>
         </div>
-        <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm '>
-          <img className='rounded-sm' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
+        <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm'>
+          <img className='rounded-sm max-h-52 object-cover' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
           <p>10 anos de experiência!</p>
         </div>
-        <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm '>
-          <img className='rounded-sm' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
+        <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm'>
+          <img className='rounded-sm max-h-52 object-cover' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
           <p>10 anos de experiência!</p>
         </div>
-        <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm '>
-          <img className='rounded-sm' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
+        <div className='bg-white p-2 flex flex-col w-full text-center space-y-2 font-inter text-lg rounded-sm'>
+          <img className='rounded-sm max-h-52 object-cover' src="https://blog.redebatista.edu.br/wp-content/uploads/2020/03/original-5163ba17ddbf11f7562a4c18a7f7cbb6.jpg" alt="" />
           <p>10 anos de experiência!</p>
         </div>
         
