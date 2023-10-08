5b91b621a497a381cd792c6b7d3157e88a0a7ae0
GabrielRodriguesMoreira <gabriel.rodrigues.moreira08@aluno.ifce.edu.br>
2023-10-08 19:49:36 -0300

    init

--- src/app/components/song/Song.jsx
+++ src/app/components/song/Song.jsx
@@ -11,13 +11,18 @@ export default function Song() {
     const [isPlayerReady, setIsPlayerReady] = useState(false);
     const [isVideoPlaying, setVideoPlaying] = useState(true);
     const playlist = ["8K875HrgVVQ", "lF2zPyyKkPA", "DhHGDOgjie4", "CarFx8c2c3c"]
-    var playlistIndex = 0
+    const [playlistIndex, setPlaylistIndex] = useState(0)
     const [songid, setsongid] = useState(null);
     const [volume, setVolume] = useState(20);
+    const constraintsRef = useRef(null);
+    const [canDrag, setCanDrag] = useState(true)
 
-    useEffect(()=>{
-        setsongid(playlist[playlistIndex])
-    },[])
+    useEffect(() => {
+        setsongid(playlist[playlistIndex]);
+        if (isPlayerReady) {
+            youtubePlayerRef.current?.setVolume(volume);
+        }
+    }, [playlistIndex])
 
     const onPlayerReady = (event) => {
         youtubePlayerRef.current = event.target;
@@ -25,11 +30,9 @@ export default function Song() {
     };
 
     const handleSwapSong = (mov) => {
-        if (playlistIndex + mov >= 0 && playlistIndex + mov <= 3) {
-            playlistIndex = playlistIndex + mov
-            setsongid(playlist[playlistIndex])
+        if (playlistIndex + mov >= 0 && playlistIndex + mov <= playlist.length) {
+            setPlaylistIndex(playlistIndex + mov);
         }
- 
     }
 
     const handleVideoPlayPause = () => {
@@ -51,6 +54,7 @@ export default function Song() {
         }
     };
 
+
     const handleCloseButtonClick = () => {
         const songContainer = document.getElementById('song-container');
         if (songContainer) {
@@ -59,50 +63,63 @@ export default function Song() {
     };
 
     return (
-        <m.main
-            id="song-container"
-            className='fixed w-60 h-12 lg:w-80 lg:h-16 bottom-3 left-3 flex items-center justify-around p-2 shadow-md shadow-zinc-900 bg-gradient-to-r from-zinc-800 via-zinc-700 to-zinc-500  rounded-md  overflow-hidden'
+        <m.div
+            ref={constraintsRef}
+            className='fixed h-screen w-screen'
             initial={{ x: '-100vw' }}
-            animate={{ x: '0' }}
             transition={{ duration: 0.8 }}
+            animate={{ x: '0' }}
         >
-            <div className='controls text-xl lg:text-2xl text-white flex items-center space-x-1'>
-                <button onClick={() => handleSwapSong(-1)}><MdKeyboardDoubleArrowLeft /></button>
-                <button onClick={handleVideoPlayPause}>
-                    {isVideoPlaying ? <BsPauseFill /> : <BsPlayFill />}
-                </button>
-                <button onClick={() => handleSwapSong(1)}><MdKeyboardDoubleArrowRight /></button>
+            <m.main
+                id="song-container"
+                className='absolute w-72 h-20 lg:w-80 lg:h-16 bottom-6 left-3 flex items-center justify-around p-2 shadow-md shadow-zinc-900 bg-gradient-to-r from-zinc-800 via-zinc-700 to-zinc-500 rounded-md overflow-hidden'
+                drag = {canDrag}
+                dragConstraints={constraintsRef}
+            >
+                <div className='controls text-xl lg:text-2xl text-white flex items-center space-x-1'>
+                    <button onClick={() => handleSwapSong(-1)}><MdKeyboardDoubleArrowLeft /></button>
+                    <button onClick={handleVideoPlayPause}>
+                        {isVideoPlaying ? <BsPauseFill /> : <BsPlayFill />}
+                    </button>
+                    <button onClick={() => handleSwapSong(1)}><MdKeyboardDoubleArrowRight /></button>
 
-                <input
-                    className='w-12 lg:w-28 cursor-pointer'
-                    type="range"
-                    min="0"
-                    max="100"
-                    value={volume}
-                    onChange={handleVolumeChange}
-                />
-            </div>
-            {songid && (
-                <div className=' relative h-full aspect-square rounded-md overflow-hidden border-slate-950 border-2'>
-                    <YouTube
-                        iframeClassName='youtube_iframe'
-                        videoId={songid}
-                        onEnd={()=>{handleSwapSong(1)}}  
-                        /*onStateChange={() =>{handleVolumeChange(volume)}} Arrumar depois */
-                        opts={{
-                            playerVars: {
-                                autoplay: 1,
-                                controls: 0,
-                                loop: 0,
-                                modestbranding: 1,
-                                volume: 20,
-                            },
-                        }}
-                        onReady={onPlayerReady}
+                    <input
+                        className='w-12 lg:w-28 cursor-pointer'
+                        type="range"
+                        min="0"
+                        max="100"
+                        value={volume}
+                        onChange={handleVolumeChange}
+                        onMouseDown={()=>{setCanDrag(false)}}
+                        onMouseUp={()=>{setCanDrag(true)}}
                     />
                 </div>
-            )}
-            <AiFillCloseCircle className='text-white text-xl cursor-pointer' onClick={handleCloseButtonClick} />
-        </m.main>
+                {songid && (
+                    <div className=' relative h-full aspect-square rounded-md overflow-hidden border-slate-950 border-2'>
+                        <YouTube
+                            iframeClassName='youtube_iframe'
+                            videoId={songid}
+                            onEnd={() => { handleSwapSong(1) }}
+                            onStateChange={() => {
+                                if (isPlayerReady) {
+                                    youtubePlayerRef.current?.setVolume(volume);
+                                }
+                            }}
+                            opts={{
+                                playerVars: {
+                                    autoplay: 1,
+                                    controls: 0,
+                                    loop: 0,
+                                    modestbranding: 1,
+                                    volume: 20,
+                                },
+                            }}
+                            onReady={onPlayerReady}
+                        />
+                    </div>
+                )}
+                <AiFillCloseCircle className='text-white text-xl cursor-pointer' onClick={handleCloseButtonClick} />
+            </m.main>
+        </m.div>
     );
 }
